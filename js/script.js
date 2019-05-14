f0396c6475bf2671d508939406fe4b7ea199b580
Ceiro Kilp <ceiro.kilp@gmail.com>
2019-05-14 22:53:13 +0300

    Fix table structure + style and js modifications

--- js/script.js
+++ js/script.js
@@ -1,34 +1,21 @@
-var answerArray = new Array(
-  "Avalanche",
-  "Human attack",
-  "Animal attack",
-  "Storm",
-  "Fire",
-  "Stealing",
-  "Hostage situation",
-  "Lost",
-  "Sickness"
-);
-
-function getAnswer() {
-  document.getElementById("answerDiv").innerHTML =
-    "<p>" +
-    answerArray[Math.floor(Math.random() * answerArray.length)] +
-    " <br>People involved: " +
-    Math.floor(Math.random() * 11) +
-    "</p>";
-}
-
 $(function() {
-  $(".heros tr:not(.accordion)").hide();
-  $(".heros tr:first-child").show();
+  var answerArray = [
+    "Avalanche",
+    "Human attack",
+    "Animal attack",
+    "Storm",
+    "Fire",
+    "Stealing",
+    "Hostage situation",
+    "Lost",
+    "Sickness"
+  ];
 
-  $(".heros tr.accordion")
-    .click(function() {
-      $(this)
-        .nextAll("tr")
-        .fadeToggle(500);
-    })
-    .eq(0)
-    .trigger("click");
+  $("#generate-situation").on("click", function(e) {
+    e.preventDefault();
+    $("#answerDiv").text(
+      answerArray[Math.floor(Math.random() * answerArray.length)]
+    );
+    $("#peopleInvolved").text(Math.floor(Math.random() * 11));
+  });
 });
