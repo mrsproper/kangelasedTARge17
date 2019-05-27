e8da77b997b402d1390f46cc914f6ed0c496229a
Asko <askokrimann@hotmail.com>
2019-05-27 20:35:02 +0300

    Saving people v1

--- js/script.js
+++ js/script.js
@@ -16,6 +16,20 @@ $(function() {
     $("#answerDiv").text(
       answerArray[Math.floor(Math.random() * answerArray.length)]
     );
-    $("#peopleInvolved").text(Math.floor(Math.random() * 11));
+    peopleCount = Math.floor(Math.random() * 11 + 1);
+    $("#peopleInvolved").text(peopleCount);
+    document.getElementById('savingButton').style.display = "inline-block";
   });
 });
+
+function savingPeople(peopleCount) {
+  var hero = $("#heroSelect option:selected").val();
+  var saved = peopleCount - Math.round(Math.random() * peopleCount);
+
+  if(hero == "default") {
+    $("#selectedHero").text("Please select Hero first!");
+  } else {
+    document.getElementById('savingButton').style.display = "none";
+    $("#selectedHero").text(hero + " saved: " + saved);
+  }    
+}
